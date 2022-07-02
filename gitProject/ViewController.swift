6ea165fd4d946a997a4b38a08264f56a234d4b7e
Hüseyin <hsynezen02@gmail.com>
2022-07-02 11:47:48 +0300

    first commit

---
+++ gitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitProject
+//
+//  Created by MacBook Pro on 2.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
